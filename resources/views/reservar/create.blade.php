8dd223072292636a99f2cca384b48eb2f422a019
josepanta <josepanta63@gmail.com>
2021-10-03 23:29:18 -0500

    Agregar en Tabla de Create Reservas

--- resources/views/reservar/create.blade.php
+++ resources/views/reservar/create.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('specific_css')
-    <!-- DataTables -->
-    <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <!-- Select2 -->
     <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
@@ -42,35 +38,25 @@
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
-                <form>
+                <form method="post" action="{{ route('reservas.store') }}">
+                  {{ csrf_field() }}
+                  <input id="tabla_array" name="tabla_array" type="hidden">
                   <div class="card-body">
                     <div class="row mt-1">
                       <div class="row ml-1 mb-3">
                           <button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#modal-agregar"><i class="fa fa-plus"></i>Agregar</button>
                       </div>
-                      <table id="reservas_table" class="table">
+                      <table id="reservas_tabla" class="table">
                           <thead>
                               <tr>
                                   <th>Habitacion</th>
+                                  <th>Usuario</th>
                                   <th>Fecha Comienzo</th>
                                   <th>Fecha Fin</th>
-                                  <th>Usuario</th>
+                                  <th>Opciones</th>
                               </tr>
                           </thead>
-                          <tbody>
-                              <tr>
-                                  <td></td>
-                                  <td></td>
-                                  <td></td>
-                                  <td>
-                                      <div class="row justify-content-between">
-                                      <button type="button" class="btn col-md-4 btn-primary btn-sm"><i class="fa fa-edit"></i >Editar</button>
-                                      <button type="button" class="btn col-md-3 btn-primary btn-sm"><i class="fa fa-eye"></i> Ver</button>
-                                      <button type="button" class="btn col-md-4 btn-primary btn-sm"><i class="fa fa-trash"></i> Eliminar</button>
-                                      </div>
-                                  </td>
-                              </tr>
-                          </tfoot>
+                          <tbody></tbody>
                       </table>
                       <div class="modal fade" id="modal-agregar">
                         <div class="modal-dialog">
@@ -82,25 +68,36 @@
                               </button>
                             </div>
                             <div class="modal-body">
-                              <div class="col">
-                                <div class="row-md-4">
-                                  <label for="habitacion"></label>
-                                  <input name=""type="text">
+                              <div class="row">
+                                <div class="form-group col-sm-6">
+                                    <label for="habitacion_agregar">Habitacion</label>
+                                    <select id="habitacion_agregar" class="form-control select">
+                                        @foreach($habitaciones as $habitacion)
+                                            <option value="{{ $habitacion->id }}">{{ $habitacion->letra_numero }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                                <div class="row">
-                                  <label for=""></label>
-                                  <input type="text">
+                                <div class="form-group col-sm-6">
+                                    <label for="user_agregar">Usuario</label>
+                                    <select id="user_agregar" class="form-control select2bs4" style="width:100%!important;">
+                                        @foreach($users as $user)
+                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                                <div class="row">
-                                  <label for=""></label>
-                                  <input type="text">
+                                <div class="form-group col-sm-6">
+                                    <label for="fecha_comienzo_agregar">Fecha Comienzo</label>
+                                    <input id="fecha_comienzo_agregar" type="date" class="form-control">
                                 </div>
-
+                                <div class="form-group col-sm-6">
+                                    <label for="fecha_fin_agregar">Fecha Fin</label>
+                                    <input id="fecha_fin_agregar" type="date" class="form-control">
+                                </div>     
                               </div>
                             </div>
                             <div class="modal-footer justify-content-between">
                               <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                              <button type="button" class="btn btn-primary">Guardar</button>
+                              <button id="guardar_modal" type="button" class="btn btn-primary" data-dismiss="modal">Guardar</button>
                             </div>
                           </div>
                           <!-- /.modal-content -->
@@ -114,7 +111,7 @@
                   <div class="card-footer">
                     <div class="d-flex justify-content-center">
                       <div class='col-sm-6'>
-                        <button type="submit" class="btn btn-primary btn-block">Agregar</button>
+                        <button id="agregar" type="submit" class="btn btn-primary btn-block">Agregar</button>
                       </div>
                     </div>
                   </div>
@@ -130,50 +127,45 @@
 @endsection
 
 @section('specific_js')
-<!-- Datatable -->
-<script src="../../dist/js/demo.js"></script>
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-
 <!-- Select2 -->
 <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
 
 <script>
-    //Initialize Select2 Elements
-    $('.select2bs4').select2({
-        theme: 'bootstrap4'
+  //Initialize Select2 Elements
+  $('.select2bs4').select2({
+      theme: 'bootstrap4'
+  });
+
+  //Navegacion
+  $(document).ready(function(){
+      $("#nav_item_reservas").addClass("menu-open");
+      $("#nav_item_title_reservas").addClass("active");
+      $("#nav_item_option_gestionar_reservas").addClass("active");
+  }); 
+
+  //Agregar Modal
+  $("#guardar_modal").click(function(){
+    $('#reservas_tabla > tbody:last-child').append("<tr><td id="+$('#habitacion_agregar').val()+">"+$("#habitacion_agregar option:selected").text()+"</td><td id="+$('#user_agregar').val()+">"+$("#user_agregar option:selected").text()+"</td><td>"+$("#fecha_comienzo_agregar").val()+"</td><td>"+$("#fecha_fin_agregar").val()+"</td><td><div class='row justify-content-between'><button type='button' class='btn col-md-4 btn-primary btn-sm'><i class='fa fa-edit'></i >Editar</button><button type='button' class='btn col-md-3 btn-primary btn-sm'><i class='fa fa-eye'></i> Ver</button><button type='button' class='btn col-md-4 btn-primary btn-sm'><i class='fa fa-trash'></i> Eliminar</button></div></td></tr>");
+  });
+
+  //Transforma a Array
+  $("#agregar").click(function(){
+    var filas = [];
+    $('#reservas_tabla tbody tr').each(function() {
+        var habitacion = $(this).find('td').eq(0).attr('id');
+        var user = $(this).find('td').eq(1).attr('id');
+        var fecha_comienzo = $(this).find('td').eq(2).text();
+        var fecha_fin = $(this).find('td').eq(2).text();
+        var fila = {
+            'habitacion': habitacion,
+            'user': user,
+            'fecha_comienzo': fecha_comienzo,
+            'fecha_fin': fecha_fin
+        };
+        filas.push(fila);
     });
 
-    //Navegacion
-    $(document).ready(function(){
-        $("#nav_item_reservas").addClass("menu-open");
-        $("#nav_item_title_reservas").addClass("active");
-        $("#nav_item_option_gestionar_reservas").addClass("active");
-    }); 
-
-    //DataTable
-    $('#reservas_habitaciones_table').DataTable({
-        "pageLength": 5,
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": false,
-        "info": false,
-        "autoWidth": false,
-        "responsive": true,
-        "language": {
-            "url": "{{ asset('plugins/language/spanish.json') }}"
-        },
-    });
+    $('#tabla_array').val(JSON.stringify(filas));
+  });
 </script>
 @endsection
